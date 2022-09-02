d11f9c2fcdd69eece639e4115d259970c79f4f5b
Asur-harsh <harshad.bkavte@gmail.com>
2022-09-02 08:00:40 +0530

    removed blank spaces

--- src/pages/fav-blogs/FavBlog.jsx
+++ src/pages/fav-blogs/FavBlog.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import Card from "../../components/card/Card";
-
 import Navigation from "../../components/nvaigation/Navigation";
 
 export default function FavBlog() {
